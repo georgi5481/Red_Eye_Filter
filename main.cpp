aeaf448d248b8a16f1b867a3ffd960dbc459581c
George <georgi5481@abv.bg>
2022-08-30 21:16:07 +0300

    added some comments

---
+++ main.cpp
@@ -0,0 +1,45 @@
+//C includes
+
+
+//C++ includes:
+#include <vector>
+#include <iostream>
+#include <cstdint>
+
+//own includes
+#include "utils/FileParser.h"
+#include "utils/Image.h"
+#include "utils/SolutionEvaluator.h"
+#include "Solution.h"
+
+
+
+int32_t main([[maybe_unused]]int32_t argc, [[maybe_unused]]char *argv[]) {
+  //Use the provided implementation that best suits your needs
+//  using Image = PackedImage;
+  using Image = StrideImage;
+
+  std::vector<Image> inputImages;
+  std::vector<Image> outputImages;
+
+  int32_t err = FileParser<Image>::generateData(inputImages, outputImages);	//gives the whole image
+
+  if (EXIT_SUCCESS != err) {							//checks for errors
+    std::cerr << "FileParser::generateData() failed" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  Solution solution;			//our solution is made as an object
+  solution.compute(inputImages);		//we call our compute method we implemented and give the input image
+
+  err = SolutionEvaluator<Image>::compare(inputImages, outputImages);			//method for printing the images and compairing them by the process
+
+  if (EXIT_SUCCESS != err) {		//checks for a missmatch at the method upwards
+    std::cout << "Solution status - [FAIL]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Solution status - [SUCCESS]" << std::endl;
+  return EXIT_SUCCESS;
+}
+
---
+++ utils/SolutionEvaluator.h
@@ -0,0 +1,35 @@
+#ifndef SOLUTIONEVALUATOR_H_
+#define SOLUTIONEVALUATOR_H_
+
+#include <cstdint>
+#include <vector>
+
+template <typename T>
+class SolutionEvaluator {
+public:
+  SolutionEvaluator() = delete;
+
+  static int32_t compare(const std::vector<T> &computed,
+                         const std::vector<T> &predefined) {
+
+    const size_t size = computed.size();
+
+    if (size != predefined.size()) {		//checks if we have different number of images as the output
+      std::cout << "Computed size: " << size << " vs Predefined size: " << predefined.size() << " mismatch" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+      if (computed[i] == predefined[i]) {		//printing the images
+        continue;
+      }
+
+      std::cout << "Computed size: vs Predefined mismatch found at imageIdx: " << i << "\nTerminating future comparisons" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;		//we printed all the images so we exit
+  }
+};
+
+#endif /* SOLUTIONEVALUATOR_H_ */
